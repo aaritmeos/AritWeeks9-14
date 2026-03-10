8270ab068de27a0a6147a3019b2ea18b01170228
sancheez <sancheez@OA-S141-16>
2026-03-10 11:55:11 -0400

    TwinStick2

--- Assets/Scripts/TwinStick.cs
+++ Assets/Scripts/TwinStick.cs
@@ -5,6 +5,8 @@ public class TwinStick : MonoBehaviour
 {
     public float speed = 5;
     public Vector2 movement;
+    public Vector2 rotation;
+    Vector3 spin;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,10 +18,20 @@ public class TwinStick : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3)movement * speed * Time.deltaTime;
+        spin = transform.eulerAngles;
+        spin.z += rotation.x * speed * Time.deltaTime;
+        transform.eulerAngles = spin;
+
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        rotation = context.ReadValue<Vector2>();
     }
 }
