e1dae574411d08240645df2f30c4761644dc430c
aaritmeos <sancheez@sheridancollege.ca>
2026-04-06 15:06:07 -0400

    Made bull move laterally without moving forward and backward using player input

--- Assets/Scripts/Bull.cs
+++ Assets/Scripts/Bull.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bull : MonoBehaviour
 {
     public float speed;
+    public Vector2 movement;
+    public Vector2 latMove;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +16,15 @@ public class Bull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.up * speed * -1 * Time.deltaTime;
+        // make bull move according to the value of Vector2 movement
+        transform.position = (Vector3)latMove;
+        latMove.x += movement.x;
+        latMove.y += speed * -1 * Time.deltaTime;
+        transform.position = latMove;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
     }
 }
